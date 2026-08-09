26c2c02a9da3a9a0a50712c81c5357c28cc20985
Altaf <altafalinadaf@gmail.com>
2026-08-09 13:13:17 +0530

    adding code for constructor reference

---
+++ constructorReference/ConstructorRefDemo.java
@@ -0,0 +1,128 @@
+package constructorReference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Employee {
+
+    String name;
+    int age;
+
+    // No-argument constructor
+    public Employee() {
+    }
+
+    // Parameterized constructor
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    // Used to print Employee object details
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Employee{");
+        sb.append("name=").append(name);
+        sb.append(", age=").append(age);
+        sb.append('}');
+
+        return sb.toString();
+    }
+}
+
+
+public class ConstructorRefDemo {
+
+    public static void main(String[] args) {
+
+        // Creating a List of employee names
+        List<String> names = Arrays.asList(
+                "Rohit",
+                "Kushl",
+                "Chetan",
+                "Mohan"
+        );
+
+
+        // --------------------------------------------------
+        // 1. Normal Constructor
+        // --------------------------------------------------
+
+        /*
+         * Creating Employee objects using
+         * the parameterized constructor.
+         *
+         * new Employee(name)
+         */
+        List<Employee> emp = new ArrayList<>();
+
+        for (String name : names) {
+
+            emp.add(new Employee(name));
+        }
+
+
+        // Printing Employee objects
+        // Method reference is used with forEach()
+        emp.forEach(System.out::println);
+
+
+        // --------------------------------------------------
+        // 2. Lambda Expression
+        // --------------------------------------------------
+
+        System.out.println("adding values using method reference");
+
+        List<Employee> emp2 = new ArrayList<>();
+
+        /*
+         * map() transforms every name into
+         * an Employee object.
+         *
+         * Lambda expression:
+         *
+         * name -> new Employee(name)
+         *
+         * For every name, a new Employee object is created.
+         */
+        emp2 = names.stream()
+                .map(name -> new Employee(name))
+                .toList();
+
+
+        // Printing Employee objects
+        emp2.forEach(System.out::println);
+
+
+        // --------------------------------------------------
+        // 3. Constructor Reference
+        // --------------------------------------------------
+
+        System.out.println("adding values using contructor reference");
+
+        List<Employee> emp3 = new ArrayList<>();
+
+        /*
+         * Constructor reference:
+         *
+         * Employee::new
+         *
+         * This is a shorter form of:
+         *
+         * name -> new Employee(name)
+         *
+         * Employee::new tells Java to use
+         * the Employee constructor.
+         */
+        emp3 = names.stream()
+                .map(Employee::new)
+                .toList();
+
+
+        // Printing Employee objects
+        emp3.forEach(System.out::println);
+    }
+}
--- methodReference/MethodRefExa.java
+++ methodReference/MethodRefExa.java
@@ -4,16 +4,84 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MethodRefExa {
-    public static void main(String[] args) {
-        List<String> name=Arrays.asList("Rafik","Mahiboob","Sohail","Kisor");
-        
-        List<String> uName=name.stream().map(str->str.toUpperCase()).toList();
-        uName.forEach(n->System.out.println(n));
 
-        // using method reference 
-        List<String> uName2=name.stream().map(String::toUpperCase).toList();
-        //we can use same for foreach method asweel
+    public static void main(String[] args) {
+
+        // Creating a List of names
+        List<String> name = Arrays.asList("Rafik", "Mahiboob", "Sohail", "Kisor");
+
+
+        // --------------------------------------------------
+        // Using Lambda Expression
+        // --------------------------------------------------
+
+        /*
+         * map() is used to transform each element.
+         *
+         * Here we are converting every name
+         * into uppercase.
+         *
+         * str -> str.toUpperCase()
+         *
+         * str is the current String object.
+         *
+         * str.toUpperCase() calls the toUpperCase()
+         * method on that String object.
+         */
+        List<String> uName = name.stream()
+                .map(str -> str.toUpperCase())
+                .toList();
+
+
+        /*
+         * forEach() is used to perform an action
+         * on every element.
+         *
+         * Here we are printing each name.
+         */
+        uName.forEach(n -> System.out.println(n));
+
+
+        System.out.println("Using method reference");
+
+
+        // --------------------------------------------------
+        // Using Method Reference
+        // --------------------------------------------------
+
+        /*
+         * Method reference is a shorter way of writing
+         * a lambda expression when the lambda only calls
+         * an existing method.
+         *
+         * Lambda:
+         *
+         * str -> str.toUpperCase()
+         *
+         * Method reference:
+         *
+         * String::toUpperCase
+         *
+         * Both perform the same operation.
+         */
+        List<String> uName2 = name.stream()
+                .map(String::toUpperCase)
+                .toList();
+
+
+        /*
+         * Method reference can also be used with forEach().
+         *
+         * Lambda:
+         *
+         * n -> System.out.println(n)
+         *
+         * Method reference:
+         *
+         * System.out::println
+         *
+         * Both print each element.
+         */
         uName2.forEach(System.out::println);
     }
-
 }
