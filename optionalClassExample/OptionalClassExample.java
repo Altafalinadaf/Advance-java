f2d7e8c8853d899b9f8f00b47f93edf9fb8da49f
Altaf <altafalinadaf@gmail.com>
2026-08-09 12:33:37 +0530

    adding Optional class Programs

---
+++ optionalClassExample/OptionalClassExample.java
@@ -0,0 +1,66 @@
+package optionClassExample;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionClassExample {
+
+    public static void main(String[] args) {
+
+        // Creating a List of names
+        List<String> name = Arrays.asList(
+                "Kartik",
+                "Rajesh",
+                "Asif",
+                "Kasim"
+        );
+
+
+        /*
+         * stream() creates a Stream from the List.
+         *
+         * filter() checks each name.
+         *
+         * contains("x") checks whether the name
+         * contains the character 'x'.
+         *
+         * None of the names contain 'x'.
+         *
+         * Therefore, findFirst() will not find any value.
+         */
+        Optional<String> n = name.stream()
+                .filter(str -> str.contains("x"))
+                .findFirst();
+
+
+        /*
+         * get() returns the value stored inside Optional.
+         *
+         * IMPORTANT:
+         *
+         * If Optional is empty, get() throws:
+         *
+         * NoSuchElementException
+         *
+         * So this line is unsafe when we are not sure
+         * whether a value exists.
+         */
+        System.out.println(n.get());
+
+
+        /*
+         * orElse() provides a default value
+         * when Optional is empty.
+         *
+         * If value exists:
+         *
+         *     value will be returned.
+         *
+         * If value does not exist:
+         *
+         *     "Not found" will be returned.
+         */
+        System.out.println(n.orElse("Not found"));
+    }
+}
---
+++ optionalClassExample/OptionalClassExample2.java
@@ -0,0 +1,78 @@
+package optionClassExample;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionClassExample2 {
+
+    public static void main(String a[]) {
+
+        // Creating a List of names
+        List<String> list = Arrays.asList("Nithin", "Sohail", "kasim");
+
+
+        /*
+         * We can use Optional to handle the situation
+         * where a value may or may not be present.
+         *
+         * findFirst() returns an Optional object.
+         *
+         * Here we are searching for a name containing "x".
+         *
+         * None of the names contain "x",
+         * so Optional will be empty.
+         */
+        Optional<String> name = list.stream()
+                            .filter(str -> str.contains("x"))
+                            .findFirst();
+
+        /*
+         * Printing Optional object.
+         *
+         * If value is present:
+         *
+         * Optional[value]
+         *
+         * If value is not present:
+         *
+         * Optional.empty
+         */
+        System.out.println(name);
+
+
+        /*
+         * orElse() is used when we want to provide
+         * a default value if Optional is empty.
+         *
+         * If name exists:
+         *     actual name will be printed.
+         *
+         * If name does not exist:
+         *     "Not found the name" will be printed.
+         */
+        System.out.println(name.orElse("Not found the name"));
+
+
+        /*
+         * We can also directly use orElse()
+         * after findFirst().
+         *
+         * Here we are searching for a name
+         * containing "h".
+         *
+         * "Nithin" contains "h".
+         *
+         * findFirst() returns:
+         * Optional[Nithin]
+         *
+         * orElse() extracts the value from Optional.
+         */
+        String name2 = list.stream()
+                           .filter(str -> str.contains("h"))
+                           .findFirst()
+                           .orElse("Not found");
+
+        System.out.println(name2);
+    }
+}
